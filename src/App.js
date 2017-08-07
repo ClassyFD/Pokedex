8289c15cfb62e3e6afbd7f283a1dcc1620c7c74f
Fernando De la Vega <fernando.delavega2847@gcssk12.net>
2017-08-06 19:39:17 -0600

    cleaned up code and made it easier to read. also added functional arrows to the mini controller. cleaned up some animations.

--- src/App.js
+++ src/App.js
@@ -1,75 +1,74 @@
 import React, { Component } from 'react';
 import getPokemonInfo from './Components/Info/service';
-import './App.css';
 import axios from 'axios';
-// import List from './Components/ListOfPokemon/List'
+import './App.css';
 class App extends Component {
-
   constructor(props){
     super(props);
-    this.state={
+      this.state={
+        pokemonToDisplay:[],
+        currentPokemonShown:[],
+        screenIsOn: false,
+        hiddenImage:true,
+        isShiny:false,
+        isExpanded: false,
+        scrolled:0,
+        urlNumber:0,
+        url: 'http://pokeapi.co/api/v2/pokemon/?offset=',
+      }
+  }
 
-      pokemonToDisplay:[],
-      screenIsOn: false,
-      currentPokemon:[],
-      hiddenImage:true,
-      isShiny:false,
-      isExpanded: false,
-    }
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.getPokemon = this.getPokemon.bind(this);
-}
-
-// initial loading of all pokemon
-// set '?limit=#' to the number of pokemon you want
-// set '?offset=#' to the number of pokemon you want to skip
-componentDidMount() {
-  let apiLink='http://pokeapi.co/api/v2/pokemon/?limit=200'
-  axios.get(apiLink)
-  .then((response)=> {
-    this.setState({
-      pokemonToDisplay: response.data.results
+  // initial loading of all pokemon
+  // set '?limit=#' to the number of pokemon you want to display
+  // set '?offset=#' to the last pokemon of the generation you want to skip
+  // Gen 1 pokemon: 1 - 151
+  // Gen 2 pokemon 152 - 251
+  // Gen 3 pokemon 252 - 386
+  // Gen 4 pokemon 387 - 493
+  // Gen 5 pokemon 494 - 649
+  // Gen 6 pokemon 650 - 721
+  // All other pokemon 721 - 810
+  componentDidMount() {
+    axios.get(this.state.url + this.state.urlNumber)
+    .then((response)=> {
+      this.setState({
+        pokemonToDisplay: response.data.results
+      })
     })
-  })       
-}
+  }
 
-// to turn the screen of the mini device on or off
-displayPokemon(){
-  !this.state.screenIsOn? 
-    this.setState({
-      screenIsOn: true
-    }): 
-    this.setState({
-    screenIsOn:false
-  });
-}
+  // to turn the screen of the mini device on or off
+  displayPokemon(){
+    !this.state.screenIsOn? 
+      this.setState({
+        screenIsOn: true
+      }): 
+      this.setState({
+      screenIsOn:false
+    });
+  }
 
-// click the show button in the mini device for the pokemon to be displayed in the pokedex.
-// also, animations!
-getPokemon(url){
-  const imageLoading = document.getElementsByClassName('sprite_size')[0].classList
-        imageLoading.add('hidden')
-        imageLoading.remove('display')
-  const default_front = document.getElementsByClassName('sprite_front')[0].classList
-        default_front.add('hide_stationary') 
-        default_front.remove('show_stationary')
+  // click the show button in the mini device for the pokemon to be displayed in the pokedex.
+  // also, animations!
+  getPokemon(url){
+    const imageLoading = document.getElementsByClassName('sprite_size')[0].classList
+      imageLoading.add('hidden')
+      imageLoading.remove('display')
+    const default_front = document.getElementsByClassName('sprite_front')[0].classList
+      default_front.add('hide_stationary') 
+      default_front.remove('show_stationary')
+    const default_back = document.getElementsByClassName('sprite_back')[0].classList
+      default_back.add('hide_back') 
+      default_back.remove('show_back')
+    const shiny_front = document.getElementsByClassName('sprite_front_shiny')[0].classList
+      shiny_front.add('hide_front') 
+      shiny_front.remove('show_front')
+    const shiny_back = document.getElementsByClassName('sprite_back_shiny')[0].classList
+      shiny_back.add('hide_back_down') 
+      shiny_back.remove('show_back_down')
 
-  const default_back = document.getElementsByClassName('sprite_back')[0].classList
-        default_back.add('hide_back') 
-        default_back.remove('show_back')
-
-  const shiny_front = document.getElementsByClassName('sprite_front_shiny')[0].classList
-        shiny_front.add('hide_front') 
-        shiny_front.remove('show_front')
-
-  const shiny_back = document.getElementsByClassName('sprite_back_shiny')[0].classList
-        shiny_back.add('hide_back_down') 
-        shiny_back.remove('show_back_down')
-
-  getPokemonInfo(url).then((response) => {
-    this.state.hiddenImage===true? this.setState({
-        hiddenImage:false
-    }): null
+      getPokemonInfo(url).then((response) => {this.state.hiddenImage===true? 
+        this.setState({hiddenImage:false}): null
         default_front.remove('hide_stationary');
         default_front.add('show_stationary');
         shiny_front.remove('hide_front');
@@ -78,103 +77,134 @@ getPokemon(url){
         default_back.add('show_back');
         shiny_back.remove('hide_back_down');
         shiny_back.add('show_back_down');
-
         imageLoading.remove('hidden')
         imageLoading.add('display')
-    this.setState({
-      currentPokemon: response,
+      this.setState({
+        currentPokemonShown: response,
+      })
     })
-  })
-}
+  }
 
-//to switch the pokemon to shiny or default!
-shinify() {
-  this.state.isShiny? 
-  this.setState({isShiny:false}): this.setState({isShiny:true})
-}
+  //to switch the pokemon to shiny or default!
+  shinify() {
+    this.state.isShiny? 
+    this.setState({isShiny:false}): this.setState({isShiny:true})
+  }
 
-//to show a bigger version of the screen!
-expandPokemon(){
-  const spriteExpand = document.getElementsByClassName('sprite')[0].classList
-  !this.state.isExpanded?  
-    spriteExpand.add('expand_animation'): 
-    spriteExpand.remove('expand_animation')
-  !this.state.isExpanded? 
-    spriteExpand.remove('hide_animation'): 
-    spriteExpand.add('hide_animation')
-  !this.state.isExpanded? 
-    this.setState({isExpanded:true}): 
-    this.setState({isExpanded: false})
-}
+  //to show a bigger version of the screen!
+  expandPokemon(){
+    const spriteExpand = document.getElementsByClassName('sprite')[0].classList
+      !this.state.isExpanded?  
+      spriteExpand.add('expand_animation') + 
+      spriteExpand.remove('hide_animation') + 
+      this.setState({isExpanded:true}): 
+      spriteExpand.remove('expand_animation') + 
+      spriteExpand.add('hide_animation') + 
+      this.setState({isExpanded: false}) 
+  }
 
-render() {
-  const show = 'Show Sprites!'
-  const hide = 'Hide Sprites!'
+  //scrolling the mini screen to the right!
+  scrollRight(){
+    this.state.scrolled === 3135?
+    axios.get(this.state.url + this.state.urlNumber).then((response)=> {
+      this.setState({
+        pokemonToDisplay: response.data.results,
+        scrolled: 165
+      })
+    }):
+    this.state.scrolled <= 3135?
+    this.setState({
+      scrolled: this.state.scrolled + 165,
+      urlNumber:  this.state.urlNumber + 1
+    }) : null
+  }
+
+  // scrolling the mini screen to the left!
+  scrollLeft(){
+    this.state.pokemonToDisplay[0].name === 'bulbasaur' && this.state.scrolled===0? null : 
+    this.state.scrolled === 0?
+    axios.get(this.state.url + (this.state.urlNumber - 19)).then((response)=>{
+      this.setState({
+        pokemonToDisplay: response.data.results,
+        scrolled: 2970
+      })
+    }) : 
+    this.state.scrolled >= 0?
+    this.setState({
+      scrolled: this.state.scrolled - 165,
+      urlNumber:  this.state.urlNumber - 1
+    }) : null 
+  }
+
+  //rendering everything!
+  render() {
     var initial = this.state.pokemonToDisplay.map((key)=>{
       return(
         <div className={this.state.screenIsOn === false? 'end': 'start'}>
-      <section>
-      <div className='name'>{key.name}</div>
-      <button className="button_show" onClick={(e)=>this.getPokemon(key.url)}> Show! </button>
-      </section>
-      </div>
-        )
-    })
-      var current =
-        ( 
-          <section>
-            {/*       notes for stats      */}
-            {/* [0] : name                 */}
-            {/* [1] : height               */}
-            {/* [2] : weight               */}
-            {/* [3] : first type           */}
-            {/* [4] : second type          */}
-            {/* [5] : front_default_sprite */}
-            {/* [6] : front_shiny_sprite   */}
-            {/* [7] : back__default_sprite */}
-            {/* [8] : back__shiny_sprite   */}
-
-          <div className="stats">
-            <div>Name: {this.state.screenIsOn? this.state.currentPokemon[0] : null}</div>
-            <div>Height: {this.state.screenIsOn? this.state.currentPokemon[1] : null}</div>
-            <div>Weight: {this.state.screenIsOn? this.state.currentPokemon[2] : null}</div>
-            <div>Type: {this.state.screenIsOn && this.state.hiddenImage === false?  
-              this.state.currentPokemon[3] + this.state.currentPokemon[4] : null}</div>
-          </div>
-          <div className='sprite'>
-            <img className="sprite_front" alt='Sprite should be here!' hidden={this.state.screenIsOn && this.state.isExpanded? 
-            this.state.hiddenImage : true} src={this.state.currentPokemon[5]}/> 
-            <img className="sprite_front_shiny" alt='this Pokemon is not shiny!' hidden={this.state.screenIsOn && this.state.isExpanded? 
-            this.state.hiddenImage : true} src={this.state.currentPokemon[6]}/> 
-            <img className="sprite_back" alt='this Pokemon is not shiny!' hidden={this.state.screenIsOn && this.state.isExpanded? 
-            this.state.hiddenImage : true} src={this.state.currentPokemon[7]}/> 
-            <img className="sprite_back_shiny" alt='this Pokemon is not shiny!' hidden={this.state.screenIsOn && this.state.isExpanded? 
-            this.state.hiddenImage : true} src={this.state.currentPokemon[8]}/> 
-            <img className="sprite_size" alt='Sprite should be here!' hidden={this.state.screenIsOn && !this.state.isExpanded? this.state.hiddenImage : true} src={this.state.isShiny? 
-            this.state.currentPokemon[6] : this.state.currentPokemon[5]}/>
-            </div>
+          <section style={{position:'relative',left: '-' + this.state.scrolled + 'px'}}>
+            <div className='name'>{key.name}</div>
+            <button className="button_show" onClick={(e)=>this.getPokemon(key.url)}> Show! </button>
           </section>
-        )
+        </div>
+      )
+    })
+      /*       notes for stats      */
+      /* [0] : name                 */
+      /* [1] : height               */
+      /* [2] : weight               */
+      /* [3] : first type           */
+      /* [4] : second type          */
+      /* [5] : front_default_sprite */
+      /* [6] : front_shiny_sprite   */
+      /* [7] : back__default_sprite */
+      /* [8] : back__shiny_sprite   */
+    var current = ( 
+      <section>
+        <div className="stats">
+          <div>Name: {this.state.screenIsOn? this.state.currentPokemonShown[0] : null}</div>
+          <div>Height: {this.state.screenIsOn? this.state.currentPokemonShown[1] : null}</div>
+          <div>Weight: {this.state.screenIsOn? this.state.currentPokemonShown[2] : null}</div>
+          <div>Type: {this.state.screenIsOn && this.state.hiddenImage === false?  
+            this.state.currentPokemonShown[3] + this.state.currentPokemonShown[4] : null}
+          </div>
+        </div>
+        <div className='sprite'>
+          <img className="sprite_front" alt='This pokemon does not have a sprite available!' hidden={this.state.screenIsOn && this.state.isExpanded? 
+          this.state.hiddenImage : true} src={this.state.currentPokemonShown[5]}/> 
+          <img className="sprite_front_shiny" alt='This pokemon does not have a shiny sprite available!' hidden={this.state.screenIsOn && this.state.isExpanded? 
+          this.state.hiddenImage : true} src={this.state.currentPokemonShown[6]}/> 
+          <img className="sprite_back" alt='This pokemon does not have a sprite available!' hidden={this.state.screenIsOn && this.state.isExpanded? 
+          this.state.hiddenImage : true} src={this.state.currentPokemonShown[7]}/> 
+          <img className="sprite_back_shiny" alt='This pokemon does not have a shiny sprite available!' hidden={this.state.screenIsOn && this.state.isExpanded? 
+          this.state.hiddenImage : true} src={this.state.currentPokemonShown[8]}/> 
+          <img className="sprite_size" alt='Sprite should be here!' hidden={this.state.screenIsOn && !this.state.isExpanded? this.state.hiddenImage : true} src={this.state.isShiny? 
+          this.state.currentPokemonShown[6] : this.state.currentPokemonShown[5]}/>
+        </div>
+      </section>
+    )
+    //putting everything in place!
     return (
       <section className="container">
-        <div className="pics">
+        <div>
           <div className='pic'>
-            <button className="expand" onClick={(e)=> this.expandPokemon(this.state.currentId)}>
-              {!this.state.isExpanded? show:hide}</button>
+            <button className="expand" onClick={(e)=>this.expandPokemon(this.state.currentId)}>
+              {!this.state.isExpanded? 'Show Sprites!':'Hide Sprites!'}
+            </button>
             <button className="shinify" onClick={(e)=> this.shinify()}>shinify!</button>
             <div style={{opacity:this.state.isShiny? 1 : .3 }} className='shinyStatus'>SHINY</div>
             <div style={{opacity:this.state.isShiny? .3 : 1 }} className='defaultStatus'>DEFAULT</div>
-              {current} 
+            {current} 
           </div>
         </div>
         <div className='smaller_pic'>
-            <button className='button_display' onClick={(e)=> this.displayPokemon(e.target.value)}> </button>
-            <div className="scroll">
+          <div className="left_arrow" onClick={(e)=>this.scrollLeft()}></div>
+          <div className="right_arrow" onClick={(e)=>this.scrollRight()}></div>
+          <button className='button_display' onClick={(e)=> this.displayPokemon(e.target.value)}> </button>
+          <div className="scroll">
             {initial}
-            </div>
           </div>
-        {/* <List/> */}
-     </section>
+        </div>
+      </section>
     );
   }
 }
--- src/Components/Info/service.js
+++ src/Components/Info/service.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
 
-
+//Getting the info for a specific pokemon
 export default function getPokemonInfo(url) {
  return axios.get(url).then((response)=> {
      var weight = response.data.weight.toString().split(''); 
      weight.splice(weight.length-1, 0, '.')
      var height = response.data.height.toString().split(''); 
      height.splice(height.length-1, 0, '.')
-     console.log(response.data)
-     return [
-            response.data.name, 
-            height.join('')+' meters',  
-            weight.join('')+'kg', 
-            response.data.types[0].type.name, 
-            ((response.data.types[1])? ', '+response.data.types[1].type.name: ''),
-            response.data.sprites.front_default,
-            response.data.sprites.front_shiny,
-            response.data.sprites.back_default,
-            response.data.sprites.back_shiny,
-          ]
+      return [
+        response.data.name, 
+        height.join('')+' meters',  
+        weight.join('')+'kg', 
+        response.data.types[0].type.name, 
+        ((response.data.types[1])? ', '+ response.data.types[1].type.name: ''),
+        response.data.sprites.front_default,
+        response.data.sprites.front_shiny,
+        response.data.sprites.back_default,
+        response.data.sprites.back_shiny,
+      ]
     }
   )
 }
